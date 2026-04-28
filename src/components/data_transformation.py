894ed723d4b6f34973a945504b59d984d36ebe89
randomlynamedwhy <randomlynamedwhy@gmail.com>
2026-04-28 08:20:04 -0500

    Fix in data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -104,6 +104,8 @@ class DataTransformation:
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
+
+            
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
